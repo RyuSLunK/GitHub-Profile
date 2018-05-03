bfb10ba18ae22cffd11f6feb1bdfae26daa48a60
Scott Lackey <slackey@inmotionsoftware.com>
2018-05-02 21:02:30 -0500

    Completed some scaffolding

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from '../home';
 import About from '../about';
-
+import Repository from '../repository';
+import Gist from '../gist';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 const App = () => (
   <div>
-    <header>
+    {/* <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
-    </header>
-
+    </header> */}
+    <AppBar position="static" color="default">
+    <Toolbar>
+      <Typography variant="title" color="inherit">
+        Home
+      </Typography>
+    </Toolbar>
+  </AppBar>
     <main>
       <Route exact path="/" component={Home} />
+      <Route path="/:repoName/commits" component={Repository} />
+      <Route path="/gists/:gistId" component={Gist} />
       <Route exact path="/about-us" component={About} />
     </main>
   </div>
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -8,36 +13,51 @@ import {
   decrement,
   decrementAsync
 } from '../../modules/counter';
+import User from '../home/user';
+import Repos from '../home/repos';
+import Gists from '../home/gists';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+};
 
 const Home = props => (
-  <div>
-    <h1>Home</h1>
-    <p>Count: {props.count}</p>
+<div>
+  <User />
+  <Repos />
+  <Gists />
+</div>
 
-    <p>
-      <button onClick={props.increment} disabled={props.isIncrementing}>
-        Increment
-      </button>
-      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>
-        Increment Async
-      </button>
-    </p>
+  // <div>
+  //   <h1>Home</h1>
+  //   <p>Count: {props.count}</p>
 
-    <p>
-      <button onClick={props.decrement} disabled={props.isDecrementing}>
-        Decrement
-      </button>
-      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>
-        Decrement Async
-      </button>
-    </p>
+  //   <p>
+  //     <button onClick={props.increment} disabled={props.isIncrementing}>
+  //       Increment
+  //     </button>
+  //     <button onClick={props.incrementAsync} disabled={props.isIncrementing}>
+  //       Increment Async
+  //     </button>
+  //   </p>
 
-    <p>
-      <button onClick={() => props.changePage()}>
-        Go to about page via redux
-      </button>
-    </p>
-  </div>
+  //   <p>
+  //     <button onClick={props.decrement} disabled={props.isDecrementing}>
+  //       Decrement
+  //     </button>
+  //     <button onClick={props.decrementAsync} disabled={props.isDecrementing}>
+  //       Decrement Async
+  //     </button>
+  //   </p>
+
+  //   <p>
+  //     <button onClick={() => props.changePage()}>
+  //       Go to about page via redux
+  //     </button>
+  //   </p>
+  // </div>
 );
 
 const mapStateToProps = state => ({
